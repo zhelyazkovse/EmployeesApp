54faba25ebdfe50247a01340d416b6229aedaf17
Keshti Gyllinger <gyllinger@gmail.com>
2025-05-15 11:14:46 +0200

    Modified program.cs

--- EmployeesApp.Web/Program.cs
+++ EmployeesApp.Web/Program.cs
@@ -1,15 +1,13 @@
-namespace EmployeesApp.Web
+namespace EmployeesApp.Web;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
-            app.Run();
-        }
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews();
+        var app = builder.Build();
+        app.MapControllers();
+        app.Run();
     }
 }
