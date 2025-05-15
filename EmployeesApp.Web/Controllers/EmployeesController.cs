1abb06e64124fe2d0d19b461379c356e74c90cb3
albertito1985 <anulo@live.se>
2025-05-15 11:58:49 +0200

    Index sida completed

---
+++ EmployeesApp.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,32 @@
+﻿using EmployeesApp.Web.Services;
+using EmployeesApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesApp.Web.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        static EmployeeService service = new EmployeeService();
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            var model = service.GetAll();
+            return View(model);
+        }
+
+        [HttpGet("/Create")]
+        public IActionResult CreateEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost("/Create")]
+        public IActionResult CreateEmployee( Employee employee)
+        {
+            service.AddEmployee(employee);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
