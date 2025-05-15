9cac5044b66529e89ded9fd993b1c50b085fb936
Keshti Gyllinger <gyllinger@gmail.com>
2025-05-15 11:32:53 +0200

    Added employee service

---
+++ EmployeesApp.Web/Services/EmployeeService.cs
@@ -0,0 +1,38 @@
+﻿using EmployeesApp.Web.Models;
+
+namespace EmployeesApp.Web.Services
+{
+    public class EmployeeService
+    {
+        private List<Employee> employees = [
+            new Employee {
+                Id = 12,
+                Name = "Anna Andersson",
+                Email = "anna84@gmail.com"
+            },
+            new Employee {
+                Id = 2,
+                Name = "Berit Bertilsson",
+                Email = "berit@gmail.com"
+            },
+            new Employee {
+                Id = 344,
+                Name = "Håkan Jonsson",
+                Email = "hakis@gmail.com"
+            }
+        ];
+
+        public void AddEmployee(Employee employee)
+        {
+            employee.Id = employees.Count == 0 ? 1 : employees.Max(d => d.Id) + 1;
+            employees.Add(employee);
+        }
+
+        public Employee[] GetAll() => employees
+            .OrderBy(d => d.Name)
+            .ToArray();
+
+        public Employee GetById(int id) => employees.Single(d => d.Id == id);
+
+    }
+}
